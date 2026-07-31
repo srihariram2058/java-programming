8149fb4e956934f94d59d1b3d6eabe1c8cc91547
srihariram2058 <srihariram2058@gmail.com>
2026-07-31 13:56:50 +0530

    Add files via upload

---
+++ control.java
@@ -0,0 +1,10 @@
+public class control {
+   public static void main(String[]args)
+   {
+   int a=11;
+    if(a>10);
+     {
+     System.out.println("A is greater than 10");
+     }
+   }
+}
---
+++ variable.java
@@ -0,0 +1,6 @@
+public class variable {
+    public static void main(String[] args) {
+byte age=20;
+System.out.println(age);
+ }
+}
