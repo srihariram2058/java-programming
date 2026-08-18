bc62a65b2f2c4144faa4b8f5f589547794e1ee1a
Sri hari <srihariram2058@gmail.com>
2026-08-18 14:51:42 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,33 @@
+public class Days {
+
+    public static void main(String[] args) {
+
+        byte days = 5;
+
+        switch (days) {
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            default:
+                System.out.println("Not Matched");
+        }
+    }
+}
